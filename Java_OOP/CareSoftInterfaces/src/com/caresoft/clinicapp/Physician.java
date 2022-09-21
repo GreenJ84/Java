d3c89e95d494cf701b304461cf7b9c49a9320d65
Jesse Greenough <jesse.greenough84@gmail.com>
2022-09-20 18:27:53 -0700

    Assignment- practice STS/ interacting with classes

---
+++ Java_OOP/CareSoftInterfaces/src/com/caresoft/clinicapp/Physician.java
@@ -0,0 +1,43 @@
+package com.caresoft.clinicapp;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Physician extends User implements HIPAACompliantUser {
+
+	public Physician(Integer id) {
+		this.id = id;
+	}
+@Override
+	public boolean assignPin(int pin) {
+		if (Integer.toString(pin).length() != 4) {
+			return false;
+		}
+		this.pin = pin;
+		return true;
+}
+
+	@Override
+	public boolean accessAuthorized(Integer confirmedAuthID) {
+		if (this.id == confirmedAuthID) {
+			return true;
+		}
+		return false;
+	}
+	
+	private ArrayList<String> patientNotes;
+	
+    // TO DO: Constructor that takes an ID
+    // TO DO: Implement HIPAACompliantUser!
+	
+    public void newPatientNotes(String notes, String patientName, Date date) {
+        String report = String.format(
+            "Datetime Submitted: %s \n", date);
+        report += String.format("Reported By ID: %s\n", this.id);
+        report += String.format("Patient Name: %s\n", patientName);
+        report += String.format("Notes: %s \n", notes);
+        this.patientNotes.add(report);
+    }
+
+	
+}
---
+++ Java_OOP/CareSoftInterfaces/src/com/caresoft/clinicapp/User.java
@@ -0,0 +1,36 @@
+package com.caresoft.clinicapp;
+
+public abstract class User {
+	protected Integer id;
+    protected int pin;
+    
+    
+	public User() {
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public int getPin() {
+		return pin;
+	}
+
+
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+	
+	
+    
+    
+}
+
+
